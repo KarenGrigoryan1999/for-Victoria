6670fbd75f498132469681358aa93d606bd47d92
KarenGrigoryan1999 <41522488+KarenGrigoryan1999@users.noreply.github.com>
2022-09-24 15:10:14 +0300

    init commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>hello world</div>
+  <MyFirstComponent v-for="item of myArray" :key="item" :fructName="item"/>
+  <button @click="click">Кликни на меня</button>
+  <p>текст</p>
+</template>
+
+<script setup>
+import MyFirstComponent from './MyFirstComponent.vue';
+
+const myArray = ['Ананас', 'Банан', 'Яблоко'];
+
+function click() {
+  alert("hello world!");
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/MyFirstComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="my-first-block">
+    <h1>{{fructName}}</h1>
+  </div>
+</template>
+
+<script setup>
+import {defineProps, toRefs} from "vue"
+
+const props = defineProps({
+    fructName: String
+});
+
+const {fructName} = toRefs(props);
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .my-first-block{
+        width: 300px;
+        background-color: silver;
+        margin-bottom: 5px;
+    }
+</style>
